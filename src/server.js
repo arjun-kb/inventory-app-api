79144bedea1d05c49a5d09eea8d20ec292345d4f
arjun-kb <72698616+arjun-kb@users.noreply.github.com>
2025-11-21 18:32:47 +0530

    feat : corse error fixed.
    - removed port number from .env

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
-
 import express from "express";
-import productsRoutes from "./routes/productsRoutes.js"
-import {connectMongoDB} from "../src/config/config-db.js"
+import productsRoutes from "./routes/productsRoutes.js";
+import { connectMongoDB } from "../src/config/config-db.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-connectMongoDB()
-//middleware req-> server-> (middle ware) -> response
-app.use(express.json())
-app.use("/api/products", productsRoutes)
+app.use(express.json());
+app.use("/api/products", productsRoutes);
 
-app.listen(process.env.PORT, () => {
+connectMongoDB().then(() => {
+  app.listen(process.env.PORT, () => {
     console.log("server is listening!!");
-})
+  });
+});
